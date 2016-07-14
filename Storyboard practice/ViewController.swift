88d86166657b225251fa5c3ac8355d2fe3732278
afallah <afallah@tfs.ca>
2016-07-14 18:55:15 -0400

    Initial Commit

---
+++ Storyboard practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storyboard practice
+//
+//  Created by Aria Fallah on 2016-07-14.
+//  Copyright © 2016 Lighthouse Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
